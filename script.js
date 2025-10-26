b3b253da8a9a620f31a92a141c3790b7be078f87
shshishi19 <shishir.cnits@gmail.com>
2025-10-26 20:23:38 +0600

    upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const inputBox = document.getElementById("input_box");
+const listContainer = document.getElementById("list_container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('You must write somthing!')
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "" ;
+    saveData();
+}
+ listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+    }
+ } ,false );
+ 
+function saveData(){
+    localStorage.setItem("data" , list_container.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
