c701bf6e0a2529026dbadbbb63be607caad3e4d7
shshishi19 <shishir.cnits@gmail.com>
2025-10-26 21:53:15 +0600

    Update HTML and CSS

--- script.js
+++ script.js
@@ -19,9 +19,11 @@ function addTask() {
  listContainer.addEventListener("click", function(e) {
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if (e.target.tagName ==="SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
  } ,false );
  
